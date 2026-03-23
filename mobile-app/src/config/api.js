07b91b47fc40c8458b6dd4ca0b7f0d93f687adcb
Oneth Kurukulasuriya <onethgk03@gmail.com>
2026-03-23 08:48:37 +0530

    Revert "feat: integrate event creation backend"

--- mobile-app/src/config/api.js
+++ mobile-app/src/config/api.js
@@ -1,8 +1,7 @@
 const ENV = {
   // Development (Local PC via USB/ADB)
   dev: {
-    //API_URL: 'http://localhost:3000/api'
-    API_URL: 'http://10.0.2.2:3000/api' 
+    API_URL: 'http://localhost:3000/api' 
   },
   
   // Production (For when deploying to Render/Railway)
